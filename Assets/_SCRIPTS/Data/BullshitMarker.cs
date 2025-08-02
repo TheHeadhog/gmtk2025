72eee4d24a57b460ec19a9ed3be0d8415fe7468f
cvettkovic <branecvele@gmail.com>
2025-08-02 12:24:42 +0200

    bs marker

---
+++ Assets/_SCRIPTS/Data/BullshitMarker.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class BullshitMarker : ScriptableObject
+    {
+        public string Message;
+        public GameTime Timestamp;
+    }
+}
