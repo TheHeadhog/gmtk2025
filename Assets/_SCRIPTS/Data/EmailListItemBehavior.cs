5eb775af66a75a53ba46f4f76c879a1bd1d8ae29
Stevan Popovic <stevan.popovic.headhog@gmail.com>
2025-08-02 17:03:01 +0200

    Different color listitems

--- Assets/_SCRIPTS/Data/EmailListItemBehavior.cs
+++ Assets/_SCRIPTS/Data/EmailListItemBehavior.cs
@@ -1,6 +1,8 @@
+using System;
 using DefaultNamespace;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EmailListItemBehavior : MonoBehaviour
 {
@@ -8,6 +10,15 @@ public class EmailListItemBehavior : MonoBehaviour
     [SerializeField] private TMP_Text header;
     [SerializeField] private TMP_Text sender;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+        GetComponent<Image>().color = transform.GetSiblingIndex() % 2 == 0 ? EmailGreenColor : EmailYellowColor;
+    }
+
+    [SerializeField] private Color EmailYellowColor;
+
+    [SerializeField] private Color EmailGreenColor;
+
     public void UpdateView(EmailData emailData)
     {
         this.emailData = emailData;
