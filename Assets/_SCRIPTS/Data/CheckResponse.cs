43ef1fb54bd81fc9031cd37e966f9bdfa1a59d3e
cvettkovic <branecvele@gmail.com>
2025-08-02 12:11:20 +0200

    + response

---
+++ Assets/_SCRIPTS/Data/CheckResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    [Serializable]
+    public class CheckResponse : ScriptableObject
+    {
+        public string Message;
+        public GameTime BeginTimestampInclusive;
+        public GameTime EndTimestampExclusive;
+    }
+}
