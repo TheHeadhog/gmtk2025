359c5fce9d2c3d57b65720a0b7bba2f8139b0f12
Lazar Cvetkovic <laki.cvetkovic@gmail.com>
2025-08-02 19:39:34 +0200

    Hippity hoppity I've added Popupity

---
+++ Assets/_SCRIPTS/UI/Popup.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Serialization;
+
+[RequireComponent(typeof(RectTransform))]
+public sealed class Popup : MonoBehaviour, IPointerDownHandler
+{
+    [SerializeField] private float openDuration = 0.4f;
+    [SerializeField] private float initialDelay = 0.4f;
+    private RectTransform rect;
+    private Canvas rootCanvas;
+    private Vector2 canvasHalfSize;
+    private Vector3 initialScale;
+    
+    private void Awake()
+    {
+        rect = GetComponent<RectTransform>();
+        rootCanvas = GetComponentInParent<Canvas>();
+        canvasHalfSize = rootCanvas.GetComponent<RectTransform>().rect.size * 0.5f;
+        initialScale = transform.localScale;
+        transform.localScale = Vector3.zero;
+    }
+    
+    private IEnumerator Start()
+    {
+        yield return new WaitForSeconds(initialDelay);
+        
+        transform.DOScale(initialScale, openDuration).SetEase(Ease.OutExpo);
+    }
+    
+    public void ClampToScreen()
+    {
+        Vector3 s = rect.lossyScale;
+        Vector2 half = new Vector2(
+            rect.rect.width  * 0.5f * s.x,
+            rect.rect.height * 0.5f * s.y);
+
+        Vector2 pos = rect.anchoredPosition;
+        pos.x = Mathf.Clamp(pos.x, -canvasHalfSize.x + half.x, canvasHalfSize.x - half.x);
+        pos.y = Mathf.Clamp(pos.y, -canvasHalfSize.y + half.y, canvasHalfSize.y - half.y);
+        rect.anchoredPosition = pos;
+    }
+
+    
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        transform.SetAsLastSibling();
+    }
+}
---
+++ Assets/_SCRIPTS/UI/PopupHeader.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(RectTransform))]
+public sealed class PopupHeader : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    [SerializeField] private Popup popup;
+    private RectTransform popupRect;
+    private Vector2 offset;
+
+    private void Awake()
+    {
+        popupRect = popup.GetComponent<RectTransform>();
+    }
+
+    public void OnBeginDrag(PointerEventData e)
+    {
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(popupRect, e.position, e.pressEventCamera,
+            out Vector2 local);
+        offset = popupRect.anchoredPosition - local;
+    }
+
+    public void OnDrag(PointerEventData e)
+    {
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(popupRect.parent as RectTransform, e.position,
+            e.pressEventCamera, out Vector2 localParent);
+        popupRect.anchoredPosition = localParent + offset;
+        popup.ClampToScreen();
+    }
+
+    public void OnEndDrag(PointerEventData e)
+    {
+    }
+}
