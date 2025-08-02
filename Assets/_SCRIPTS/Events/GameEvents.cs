f4da4606d8a27fdbba411ac573eba784797e2628
cvettkovic <branecvele@gmail.com>
2025-08-02 13:56:51 +0200

    on info marker appear & manager

--- Assets/_SCRIPTS/Events/GameEvents.cs
+++ Assets/_SCRIPTS/Events/GameEvents.cs
@@ -6,8 +6,10 @@ public static class GameEvents
     public static event Action<int> GameTimeChanged;
     public static event Action<CheckResponse> OnBadResponse;
     public static event Action<CheckResponse> OnGoodResponse;
+    public static event Action<InfoMarker> OnInfoMarkerAppear;
 
     public static void RaiseGameTimeChanged(int gameTime) => GameTimeChanged?.Invoke(gameTime);
     public static void RaiseBadResponse(CheckResponse response) => OnBadResponse?.Invoke(response);
     public static void RaiseGoodResponse(CheckResponse response) => OnGoodResponse?.Invoke(response);
+    public static void RaiseInfoMarkerAppear(InfoMarker marker) => OnInfoMarkerAppear?.Invoke(marker);
 }
---
+++ Assets/_SCRIPTS/_Managers/InfoManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DefaultNamespace;
+using UnityEngine;
+
+namespace _Managers
+{
+    public class InfoManager : SingletonPersistent<InfoManager>
+    {
+        [SerializeField] private List<InfoMarker> allInfoMarkers;
+        
+        public void SetNewMarker(InfoMarker marker) => allInfoMarkers.Add(marker);
+
+        private void OnEnable()
+        {
+            GameEvents.GameTimeChanged += CheckGameTick;
+        }
+        
+        private void OnDisable()
+        {
+            GameEvents.GameTimeChanged -= CheckGameTick;
+        }
+
+        private void CheckGameTick(int gameTick)
+        {
+            var infoMarkers = allInfoMarkers.Where(m => m.Timestamp.ToGameTick() == gameTick).ToList();
+
+            if (infoMarkers.Count == 0)
+            {
+                return;
+            }
+
+            foreach (var infoMarker in infoMarkers)
+            {
+                GameEvents.RaiseInfoMarkerAppear(infoMarker);
+            }
+        }
+    }
+}
