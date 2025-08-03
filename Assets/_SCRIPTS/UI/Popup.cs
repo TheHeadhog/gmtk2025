e1967ac84de118f2127c6750e0b3b5f5c77b509d
Lazar Cvetkovic <laki.cvetkovic@gmail.com>
2025-08-03 16:58:37 +0200

    Spammy popup fix

--- Assets/_SCRIPTS/UI/Popup.cs
+++ Assets/_SCRIPTS/UI/Popup.cs
@@ -51,6 +51,7 @@ public sealed class Popup : MonoBehaviour, IPointerDownHandler
     {
         yield return new WaitForSeconds(initialDelay);
 
+        transform.SetAsLastSibling();
         transform.DOScale(initialScale, openDuration).SetEase(Ease.OutExpo).OnComplete(() => OnOpened?.Invoke());
     }
     
